49d7672d7c07f638ef595223646a5c115b9974fd
chetui <yuxinjiect@gmail.com>
2014-08-04 20:13:45 +0800

    add task divide

--- example/base_example.cpp
+++ example/base_example.cpp
@@ -67,7 +67,7 @@ void myscheduler(HOST *host, std::vector<VM> &vms)
         //INPUT: available info
 
             //<<host static info>>
-                //OP_HS_NODE_CORE_HPTHREAD
+                //OP_HS_NODE_CORE_HPTHREAD //Yu
                 host->node_num(); //DONE
                 host->node_ids();
                 host->node_id(core_id); //core_id is a struct with 2 int. It is combined with node_id & core_id.
@@ -83,74 +83,74 @@ void myscheduler(HOST *host, std::vector<VM> &vms)
                 host->hpthread_ids();
                 host->hpthread_ids(node_id); //DONE
                 host->hpthread_ids(core_id);
-                //OP_HS_TOTAL_MEM_SIZE ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_HS_TOTAL_MEM_SIZE ((( OP_HS_NODE_CORE_HPTHREAD //Zuo
                 host->total_mem_size();
                 host->total_mem_size(node_id);
-                //OP_HS_SYS_NODE_DIST ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_HS_SYS_NODE_DIST ((( OP_HS_NODE_CORE_HPTHREAD //Yu
                 host->sys_node_dist(); //DONE
                 host->sys_node_dist(node_id_0, node_id_1); //DONE
-                //OP_HS_TEST_NODE_DIST ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_HS_TEST_NODE_DIST ((( OP_HS_NODE_CORE_HPTHREAD //Yu
                 host->test_node_dist(); //DONE
                 host->test_node_dist(node_id_0, node_id_1); //DONE
 
             //<<host dynamic info>>
-                //OP_HS_CPU_REUSE_RATIO ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_HS_CPU_REUSE_RATIO ((( OP_HS_NODE_CORE_HPTHREAD /Yu
                 host->cpu_reuse_ratio();
                 host->cpu_reuse_ratio(node_id);
-                //OP_HS_CPU_USAGE ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_HS_CPU_USAGE ((( OP_HS_NODE_CORE_HPTHREAD //Zuo
                 host->cpu_usage();
                 host->cpu_usage(node_id);
                 host->cpu_usage(core_id);
                 host->cpu_usage(hpthread_id);
-                //OP_HS_USED_MEM_SIZE ((( OP_HS_NODE_CORE_HPTHREAD
-                host->used_mem_size(); //DONE Zuo
-                host->used_mem_size(node_id); //DONE Zuo
+                //OP_HS_USED_MEM_SIZE ((( OP_HS_NODE_CORE_HPTHREAD //Zuo
+                host->used_mem_size(); //DONE
+                host->used_mem_size(node_id); //DONE
 
             //<<VM static info>>
-                //OP_VM_VCPU_VMTHREAD
+                //OP_VM_VCPU_VMTHREAD //Yu
                 vm.vm_id();
                 vm.vmthread_num();
                 vm.vmthread_ids(); //need to check whether some threads would be created, which make vmthread_num & vmthread_ids to be dynamic info.
                 vm.vcpu_num();
                 vm.vcpu_ids();
-                //OP_VM_VNODE ((( OP_VM_VCPU_VMTHREAD 
-                vm.vcpu_ids(vnode_id); //vNUMA //DONE Zuo
-                vm.vnode_num(); //vNUMA //DONE Zuo
-                vm.vnode_ids(); //vNUMA //DONE Zuo
-                //OP_VM_TOTAL_MEM_SIZE
+                //OP_VM_VNODE ((( OP_VM_VCPU_VMTHREAD //Zuo
+                vm.vcpu_ids(vnode_id); //vNUMA //DONE
+                vm.vnode_num(); //vNUMA //DONE
+                vm.vnode_ids(); //vNUMA //DONE
+                //OP_VM_TOTAL_MEM_SIZE //Zuo
                 vm.total_mem_size();
-                //OP_VM_MEM_POLICY ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_VM_MEM_POLICY ((( OP_HS_NODE_CORE_HPTHREAD //Zuo
                 vm.mem_policy(); //memory policy is static currently, since it is hard to implement dynamicly
                 vm.bindinfo_mem_node_ids();
 
             //<<VM dynamic info>>
-                //OP_VM_CPU_BINDINFO ((( OP_VM_VCPU_VMTHREAD, OP_HS_NODE_CORE_HPTHREAD
+                //OP_VM_CPU_BINDINFO ((( OP_VM_VCPU_VMTHREAD, OP_HS_NODE_CORE_HPTHREAD //Zuo
                 vm.bindinfo_hpthread_ids();
                 vm.bindinfo_hpthread_ids(vcpu_id);
                 vm.bindinfo_hpthread_ids(vmthread_id);
-                //OP_VM_MEM_BINDINFO ((( OP_VM_VNODE, OP_HS_NODE_CORE_HPTHREAD
+                //OP_VM_MEM_BINDINFO ((( OP_VM_VNODE, OP_HS_NODE_CORE_HPTHREAD //Zuo
                 vm.bindinfo_mem_node_id(vnode_id); //vNUMA
-                //OP_VM_CPU_USAGE ((( OP_VM_VCPU_VMTHREAD
+                //OP_VM_CPU_USAGE ((( OP_VM_VCPU_VMTHREAD //Yu
                 vm.cpu_usage();
                 vm.cpu_usage(vcpu_id);
                 vm.cpu_usage(vmthread_id);
-                //OP_VM_MISS_RATE ((( OP_VM_VCPU_VMTHREAD
+                //OP_VM_MISS_RATE ((( OP_VM_VCPU_VMTHREAD //Yu
                 vm.miss_rate(MISS_RATE_TYPE);
                 vm.miss_rate(MISS_RATE_TYPE, vcpu_id);
                 vm.miss_rate(MISS_RATE_TYPE, vmthread_id);
-                //OP_VM_MEM_SAMPLE
+                //OP_VM_MEM_SAMPLE //Yu
                 vm.mem_sample(); //sample the latest visited page addr
-                //OP_VM_USED_MEM_SIZE ((( OP_HS_NODE_CORE_HPTHREAD
+                //OP_VM_USED_MEM_SIZE ((( OP_HS_NODE_CORE_HPTHREAD //Zuo
                 vm.used_mem_size();
                 vm.used_mem_size(node_id);
 
         //OUTPUT: decide scheduling strategy
 
-                framework->set_vcpu_mig(vcpu_id, hpthread_ids);
-                framework->set_vcpu_mig(vmthread_id, hpthread_ids);
-                framework->set_mem_mig(vm_id, node_ids);
-                framework->set_mem_mig(vm_id, node_ids, addr_start, page_size);
-                framework->set_mem_mig(vnode_id, node_id); //vNUMA
+                framework->set_vcpu_mig(vcpu_id, hpthread_ids); //Yu
+                framework->set_vcpu_mig(vmthread_id, hpthread_ids); //Yu
+                framework->set_mem_mig(vm_id, node_ids); //Zuo
+                framework->set_mem_mig(vm_id, node_ids, addr_start, page_size); //Zuo
+                framework->set_mem_mig(vnode_id, node_id); //vNUMA //Zuo
     }
 
     return;
